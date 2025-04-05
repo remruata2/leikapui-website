7571bdd761d23a4b58b90cac568d31acd4027dfa
Remruata <erp@mzhssp.in>
2025-04-05 20:28:43 +0530

    mobile check

--- src/views/MainPages/OTTPage.jsx
+++ src/views/MainPages/OTTPage.jsx
@@ -1,4 +1,4 @@
-import { memo, Fragment } from "react";
+import { memo, Fragment, useEffect } from "react";
 // hero slider
 import OttHeroSlider from "../../components/slider/OttHeroSlider";
 
@@ -11,16 +11,26 @@ import PopularMovies from "../../components/sections/PopularMovies";
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-console.log("Test OttPage renders");
+console.log("Test OTTPage renders");
+console.log("Device type:", isMobile ? "Mobile" : "Desktop");
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const HomePage = memo(() => {
+  useEffect(() => {
+    // Log when component mounts
+    console.log("OTTPage mounted");
+    console.log("Environment variables:", {
+      apiUrl: import.meta.env.VITE_API_URL,
+      clientUrl: import.meta.env.VITE_CLIENT_URL,
+    });
+  }, []);
+
   return (
     <Fragment>
-      {!isMobile && (
-        <>
-          <OttHeroSlider />
-          <PopularMovies paddingY="my-4" />
-        </>
-      )}
+      <>
+        <OttHeroSlider />
+        <PopularMovies paddingY="my-4" />
+      </>
       {/* <TvShowsSlider /> */}
     </Fragment>
   );
